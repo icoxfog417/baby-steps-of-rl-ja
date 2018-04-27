e9d7c2bb3f030a517f4bfc82f8bc712ad779e8cb
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-04-27 14:55:42 +0900

    fix parameter

--- FN/dqn_agent.py
+++ FN/dqn_agent.py
@@ -192,7 +192,6 @@ def main(play, is_test):
     else:
         env = gym.make("Catcher-v0")
         obs = CatcherObserver(env, 80, 80, 4)
-        trainer.learning_rate = 9e-4
         trainer.initial_epsilon = 0.2
 
     if play:
