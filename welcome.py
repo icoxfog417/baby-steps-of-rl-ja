8f4e34bb1cd0b8cb18d3764fc2cd87cd317d1be6
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-07-13 10:23:21 +0900

    add welcome code

---
+++ welcome.py
@@ -0,0 +1,36 @@
+import numpy as np
+from tensorflow.python import keras as K
+import gym
+import gym_ple
+
+
+def welcome():
+    """
+    Code to check installation of basic libraries
+    """
+
+    env = gym.make("Catcher-v0")
+    num_action = env.action_space.n
+    episode_count = 10
+
+    s = env.reset()
+    brain = K.Sequential()
+    brain.add(K.layers.Dense(num_action, input_shape=[np.prod(s.shape)],
+                             activation="softmax"))
+
+    def policy(s):
+        evaluation = brain.predict(np.array([s.flatten()]))
+        return np.argmax(evaluation)
+
+    for e in range(episode_count):
+        s = env.reset()
+        done = False
+        while not done:
+            env.render()
+            a = policy(s)
+            n_state, reward, done, info = env.step(a)
+            s = n_state
+
+
+if __name__ == "__main__":
+    welcome()
