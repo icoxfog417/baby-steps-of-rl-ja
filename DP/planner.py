eded3fe733ca321a95bdb0819e00d6aaa3eb4a16
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-07-19 18:03:51 +0900

    change order

--- DP/planner.py
+++ DP/planner.py
@@ -8,15 +8,15 @@ class Planner():
         self.env.reset()
         self.log = []
 
+    def plan(self, gamma=0.9, threshold=0.0001):
+        raise Exception("Planner have to implements plan method.")
+
     def transitions_at(self, state, action):
         transition_probs = self.env.transit_func(state, action)
         for next_state in transition_probs:
             prob = transition_probs[next_state]
             reward, _ = self.env.reward_func(next_state)
             yield prob, next_state, reward
-
-    def plan(self, gamma=0.9, threshold=0.0001):
-        raise Exception("Planner have to implements plan method.")
 
     def dict_to_grid(self, state_reward_dict):
         grid = []
