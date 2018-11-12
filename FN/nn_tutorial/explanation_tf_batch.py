7a5459e93e0c8924c76742e6c2fd9843e6c2fc4c
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-11-12 16:15:49 +0900

    update tutorial

--- FN/nn_tutorial/explanation_tf_batch.py
+++ FN/nn_tutorial/explanation_tf_batch.py
@@ -1,31 +1,31 @@
 import numpy as np
 import tensorflow as tf
 
-# Weight (row=4 x col=2)
+# Weight (row=4 x col=2).
 a = tf.Variable(np.random.rand(4, 2))
 
-# Bias (row=4 x col=1)
+# Bias (row=4 x col=1).
 b = tf.Variable(np.random.rand(4, 1))
 
-# Input(x) (row=2 x col=1)
+# Input(x) (row=2 x col=1).
 x = tf.placeholder(tf.float64, shape=(2, 1))
 
-# Output(y) (row=4 x col=1)
+# Output(y) (row=4 x col=1).
 y = tf.matmul(a, x) + b
 
 
 with tf.Session() as sess:
-    # Initialize variable
+    # Initialize variable.
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    # Make batch
+    # Make batch.
     batch = []
     for i in range(3):
         x_value = np.random.rand(2, 1)
         batch.append(x_value)
 
-    # Execute culculation
+    # Execute culculation.
     y_outputs = []
     for x_value in batch:
         y_output = sess.run(y, feed_dict={x: x_value})
